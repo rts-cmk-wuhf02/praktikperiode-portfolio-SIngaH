ba9b544f9de8b3a6f421baa91dd67b6d7b4869e4
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-06-11 14:23:55 +0200

    drop down

---
+++ src/js/examples-drop-down.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    
+    document.getElementById("drop-down-icon").addEventListener("click", ()=>{
+        let dropDowns = document.getElementById("drop-down");
+        let icon = document.getElementById("drop-down-icon");
+        
+        
+        if(dropDowns.style.display === "block"){
+            dropDowns.style.display="none" 
+            icon.style.transform= "rotate(360deg)"
+        }else{
+            dropDowns.style.display="block"
+            icon.style.transform= "rotate(180deg)"
+        }
+        
+    });
+});
