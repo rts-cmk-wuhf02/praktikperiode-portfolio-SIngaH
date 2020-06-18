96f9e2ab574395f3a43f996f4e7237892ec8c8ea
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-06-18 09:56:49 +0200

    lazy-loading

---
+++ src/js/lazy-load.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const imgs = document.querySelectorAll("img");
+
+    const options = {
+        rootMargin: "0px 0px 0px 0px",
+        threshold: "0"
+    };
+
+    const imageCallback = function(entries, self){
+        entries.forEach(entry => {
+            console.log(entry);
+            if(entry.isIntersecting) {
+                entry.target.src = entry.target.dataset.src;
+            }
+        })
+    }
+
+    const ImageObserver = new IntersectionObserver(imageCallback, options);
+
+    imgs.forEach(img => {
+        ImageObserver.observe(img);
+    });
+
+});
